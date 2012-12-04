d18d1e8b6f2d4faa32a5810fb4859acf7ec0d4d6
rwunder <robert.wunderlich@mavs.uta.edu>
2012-12-04 15:51:06 -0600

    DId it work?

--- CSE 4340 - Term Project/src/com/cse4340/appointment/maker/MainActivity.java
+++ CSE 4340 - Term Project/src/com/cse4340/appointment/maker/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Jacky was here too
         
         /*	Initiator Device:
          *	1.	Identify which devices are "invited" (UI - Jacky)
