fa7df3f4aaa9a32094f525cf50832b97a3e659da
jackytp <jackytrevireyes@gmail.com>
2012-12-04 15:38:55 -0600

    Trying to commit

--- CSE 4340 - Term Project/src/com/cse4340/appointment/maker/MainActivity.java
+++ CSE 4340 - Term Project/src/com/cse4340/appointment/maker/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // Jacky was here
+        // Jacky was here twices
         
         /*	Initiator Device:
          *	1.	Identify which devices are "invited" (UI - Jacky)
