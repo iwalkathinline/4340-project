2b878836486e59cb8f2fbe5c181f96abe11670ef
jackytp <jackytrevireyes@gmail.com>
2012-12-04 15:46:06 -0600

    Last time

--- CSE 4340 - Term Project/src/com/cse4340/appointment/maker/MainActivity.java
+++ CSE 4340 - Term Project/src/com/cse4340/appointment/maker/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // Jacky was here twices
+        // No more
         
         /*	Initiator Device:
          *	1.	Identify which devices are "invited" (UI - Jacky)
