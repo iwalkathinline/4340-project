399d5b867c37d37914a32ecd3efa0577ea9d16e9
jackytp <jackytrevireyes@gmail.com>
2012-12-08 21:36:07 -0600

    Problems commiting

---
+++ src/com/finalproject/SelectAttendees.java
@@ -0,0 +1,179 @@
+package com.finalproject;
+
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass.Device;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SelectAttendees extends Activity {
+	
+	private static final int REQUEST_ENABLE_BT = 1;
+	
+	//GUI 
+	ListView listDevicesFound;
+	Button btnScanDevice;
+	Button btnNext;
+	TextView stateBluetooth;
+	
+	BluetoothAdapter bluetoothAdapter;
+	//List that holds all scaned devices
+	public static ArrayList<BluetoothDevice> allDevices = new ArrayList<BluetoothDevice>();
+	//List that holds selected devices
+	public static ArrayList<BluetoothDevice> selectedDevices = new ArrayList<BluetoothDevice>();
+	 
+	ArrayAdapter<String> btArrayAdapter;
+ 
+ /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_attendees);
+        
+        
+        btnNext = (Button)findViewById(R.id.selectTimes);
+        stateBluetooth = (TextView)findViewById(R.id.bluetoothstate);
+        
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        
+        //Prepare the list that will hold paired devices
+        //GUI
+        btnScanDevice = (Button)findViewById(R.id.scandevice);
+        listDevicesFound = (ListView)findViewById(R.id.devicesfound);
+        btArrayAdapter = new ArrayAdapter<String>(SelectAttendees.this, android.R.layout.simple_list_item_multiple_choice);
+        listDevicesFound.setAdapter(btArrayAdapter);
+        listDevicesFound.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        
+        //Check what is the state of bluetooth
+        CheckBlueToothState();
+        
+        //Button's Listeners
+        btnScanDevice.setOnClickListener(btnScanDeviceOnClickListener);
+        btnNext.setOnClickListener(btnNextOnClickListener);
+        
+        registerReceiver(ActionFoundReceiver, 
+          new IntentFilter(BluetoothDevice.ACTION_FOUND));
+    }
+    
+     @Override
+	 protected void onDestroy() {
+	  // TODO Auto-generated method stub
+	  super.onDestroy();
+	  unregisterReceiver(ActionFoundReceiver);
+	 }
+     
+    
+     
+     
+	 private void CheckBlueToothState(){
+	     if (bluetoothAdapter == null){
+	         stateBluetooth.setText("Bluetooth NOT support");
+	        }else{
+	         if (bluetoothAdapter.isEnabled()){
+	          if(bluetoothAdapter.isDiscovering()){
+	           stateBluetooth.setText("Bluetooth is currently in device discovery process.");
+	          }else{
+	           stateBluetooth.setText("Bluetooth is Enabled.");
+	           btnScanDevice.setEnabled(true);
+	          }
+	         }else{
+	          stateBluetooth.setText("Bluetooth is NOT Enabled!");
+	          Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+	         }
+	        }
+	    }
+    
+	 /** Button Next Listener
+	  *  Description: When button "Next" is clicked, the selected bluetooth devices
+	  *  	will be stored in the selectedDevices array, then it will be directed
+	  *  	to setup_meeting activity 
+	  */
+	 private Button.OnClickListener btnNextOnClickListener 
+	     = new Button.OnClickListener(){
+
+			  @Override
+			  public void onClick(View arg0) {
+				
+				  //Count the number of devices found as paired
+				  //and go trough everyone to check if it is selected (or checked)
+				  int count = listDevicesFound.getAdapter().getCount();
+				  for (int i = 0; i < count; i++) {
+					  //isItemChecked returns boolean
+					  if (listDevicesFound.isItemChecked(i)) {
+						  //If device it is selected, then store it in selectedDevices 
+						  //How: Go trought the pairedDevices Set and select the device that is 
+						  //in the i position. Add it to the selectedPairedDevices array
+						  selectedDevices.add(allDevices.get(i));
+							              
+					  }
+				  } 
+				
+				//Debug
+					 String names = "";
+					 for (BluetoothDevice device : selectedDevices) {
+					       String deviceBTName = device.getName();
+					       names += deviceBTName + "/n"; 
+					 }
+					 Toast msg = Toast.makeText(SelectAttendees.this, names, Toast.LENGTH_LONG);
+
+					 msg.setGravity(Gravity.CENTER, msg.getXOffset() / 2, msg.getYOffset() / 2);
+
+					 msg.show();
+			  }
+	 };
+	 
+	/** Button Scan Device Listener
+	 *  Description: When button is clicked, the ListView will be cleared
+	 *  	and the discovery of bluetooth devices will start 
+	 */
+    private Button.OnClickListener btnScanDeviceOnClickListener
+    = new Button.OnClickListener(){
+    	
+		  @Override
+		  public void onClick(View arg0) {
+		   // TODO Auto-generated method stub
+		   btArrayAdapter.clear();
+		   bluetoothAdapter.startDiscovery();
+		  }
+  };  
+  
+
+
+ @Override
+ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+  // TODO Auto-generated method stub
+  if(requestCode == REQUEST_ENABLE_BT){
+   CheckBlueToothState();
+  }
+ }
+    
+ private final BroadcastReceiver ActionFoundReceiver = new BroadcastReceiver(){
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+   // TODO Auto-generated method stub
+   String action = intent.getAction();
+   if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+             BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+             btArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+             btArrayAdapter.notifyDataSetChanged();
+             allDevices.add(device);
+         }
+  }};
+    
+}
---
+++ src/com/finalproject/Setup_meeting.java
@@ -0,0 +1,256 @@
+package com.finalproject;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class Setup_meeting extends Activity {
+
+/* Date Variables */
+private TextView startDateDisplay;
+private TextView endDateDisplay;
+private Button startPickDate;
+private Button endPickDate;
+private int fromYear, fromMonth, fromDay;
+private int toYear, toMonth, toDay;
+
+
+static final int DATE_PICKER_TO = 0;
+static final int DATE_PICKER_FROM = 1;
+
+
+/* Time Variables */
+private TextView startTimeDisplay;
+private TextView endTimeDisplay;
+private Button startPickTime;
+private Button endPickTime;
+private int fromHour, fromMin;
+private int toHour, toMin;
+
+static final int TIME_PICKER_TO = 2;
+static final int TIME_PICKER_FROM = 3;
+
+
+/** Called when the activity is first created. */
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.setup_meeting);
+
+    /* ================ Calendar ================ */
+    /*  capture our View elements for the start date function   */
+    startDateDisplay = (TextView) findViewById(R.id.startdateDisplay);
+    startPickDate = (Button) findViewById(R.id.startpickDate);
+
+    /* add a click listener to the button   */
+    startPickDate.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            showDialog(DATE_PICKER_FROM);
+        }
+    });
+
+    /* get the current date */
+    final Calendar c = Calendar.getInstance();
+    fromYear = c.get(Calendar.YEAR);
+    fromMonth = c.get(Calendar.MONTH);
+    fromDay = c.get(Calendar.DAY_OF_MONTH);
+
+    /* display the current date (this method is below)  */
+    updateStartDisplay();
+
+
+ /* capture our View elements for the end date function */
+    endDateDisplay = (TextView) findViewById(R.id.enddateDisplay);
+    endPickDate = (Button) findViewById(R.id.endpickDate);
+
+    /* add a click listener to the button   */
+    endPickDate.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            showDialog(DATE_PICKER_TO);
+        }
+    });
+
+    /* get the current date */
+    final Calendar c1 = Calendar.getInstance();
+    toYear = c1.get(Calendar.YEAR);
+    toMonth = c1.get(Calendar.MONTH);
+    toDay = c1.get(Calendar.DAY_OF_MONTH);
+
+    /* display the current date (this method is below)  */
+    updateEndDisplay();
+    
+    /* ================ Time ================ */
+    startTimeDisplay = (TextView) findViewById(R.id.startTimeDisplay);
+    startPickTime = (Button) findViewById(R.id.startPickTime);
+    
+    startPickTime.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            showDialog(TIME_PICKER_FROM);
+        }
+    });
+    
+    final Calendar c2 = Calendar.getInstance();
+    fromHour = c2.get(Calendar.HOUR_OF_DAY);
+    fromMin = c2.get(Calendar.MINUTE);
+    
+    updateStartTimeDisplay();
+    
+    endTimeDisplay = (TextView) findViewById(R.id.endTimeDisplay);
+    endPickTime = (Button) findViewById(R.id.endPickTime);
+    
+    endPickTime.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            showDialog(TIME_PICKER_TO);
+        }
+    });
+    
+    final Calendar c3 = Calendar.getInstance();
+    toHour = c3.get(Calendar.HOUR_OF_DAY);
+    toMin = c3.get(Calendar.MINUTE);
+    
+    updateEndTimeDisplay();
+    
+    
+}
+
+private void updateEndTimeDisplay() {
+	endTimeDisplay.setText(
+			new StringBuilder()
+			.append(pad(toHour)).append(":")
+			.append(pad(toMin)));
+}
+
+private void updateStartTimeDisplay() {
+	startTimeDisplay.setText(
+			new StringBuilder()
+			.append(pad(fromHour)).append(":")
+			.append(pad(fromMin)));
+}
+
+private void updateEndDisplay() {
+    endDateDisplay.setText(
+            new StringBuilder()
+                // Month is 0 based so add 1
+                .append(toMonth + 1).append("-")
+                .append(toDay).append("-")
+                .append(toYear).append(" "));
+
+}
+
+
+
+private void updateStartDisplay() {
+    startDateDisplay.setText(
+            new StringBuilder()
+                // Month is 0 based so add 1
+                .append(fromMonth + 1).append("-")
+                .append(fromDay).append("-")
+                .append(fromYear).append(" "));
+
+
+}
+
+private DatePickerDialog.OnDateSetListener from_dateListener =
+new DatePickerDialog.OnDateSetListener() {
+
+public void onDateSet(DatePicker view, int year, 
+                      int monthOfYear, int dayOfMonth) {
+    fromYear = year;
+    fromMonth = monthOfYear;
+    fromDay = dayOfMonth;
+    updateStartDisplay();
+}
+};
+
+
+private DatePickerDialog.OnDateSetListener to_dateListener =
+new DatePickerDialog.OnDateSetListener() {
+
+public void onDateSet(DatePicker view, int year, 
+                      int monthOfYear, int dayOfMonth) {
+    toYear = year;
+    toMonth = monthOfYear;
+    toDay = dayOfMonth;
+    updateEndDisplay();
+}
+};
+
+private TimePickerDialog.OnTimeSetListener from_timeListener =
+new TimePickerDialog.OnTimeSetListener() {
+	
+	public void onTimeSet(TimePicker view, int hourOfDay, int minute){
+		fromHour = hourOfDay;
+		fromMin = minute;
+		updateStartTimeDisplay();
+	}
+	
+};
+
+private TimePickerDialog.OnTimeSetListener to_timeListener =
+new TimePickerDialog.OnTimeSetListener() {
+	
+	public void onTimeSet(TimePicker view, int hourOfDay, int minute){
+		toHour = hourOfDay;
+		toMin = minute;
+		updateEndTimeDisplay();
+	}
+	
+};
+
+
+@Override
+protected Dialog onCreateDialog(int id) {
+	switch (id) {
+		case DATE_PICKER_FROM:
+			return new DatePickerDialog(this,
+			        from_dateListener,
+			        fromYear, fromMonth, fromDay);
+		case DATE_PICKER_TO:
+			return new DatePickerDialog(this,
+			        to_dateListener,
+			        toYear, toMonth, toDay);
+		case TIME_PICKER_FROM:
+			return new TimePickerDialog(this,
+					from_timeListener, 
+					fromHour, fromMin, false);
+		case TIME_PICKER_TO:
+			return new TimePickerDialog(this,
+					to_timeListener, 
+					toHour, toMin, false);
+			
+	}
+	return null;
+}
+
+
+private static String pad(int c) {
+    if (c >= 10)
+        return String.valueOf(c);
+    else
+        return "0" + String.valueOf(c);
+}
+
+public void selectPairedDevices(View view) {
+	Intent intent = new Intent(this, ListPairedDevices.class);
+	startActivity(intent);
+}
+
+public void scanDevices(View view) {
+	Intent intent = new Intent(this, SelectAttendees.class);
+	startActivity(intent);
+}
+
+
+
+
+}
